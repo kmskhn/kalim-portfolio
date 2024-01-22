cd9a4202e56e6ed641a1c55347d8a495082d35b2
kalimpcln <kalimullah.khan@priceline.com>
2024-01-22 16:14:13 +0530

    added line break

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-white ">
-        Software Engineer Level 2 at Priceline.com (Booking Holdings)
+        Software Engineer Level 2 at<br/> Priceline.com (Booking Holdings)
         </h2>
       </div>
     </div>
